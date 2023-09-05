717e053139bab2728cca0ccbcaff4297b5c5e08c
matiasnav710 <navarretematias710@gmail.com>
2023-09-05 17:00:18 +0500

    performed migrations and setup fix

--- src/start/datasource.ts
+++ src/start/datasource.ts
@@ -1,13 +1,16 @@
 import { DataSource } from "typeorm"
 import { isProduction } from "../constants/constants"
+import envConfig from "./envConfig"
+envConfig()
 
 const datasource = new DataSource({
     type: "postgres",
     host: process.env.PG_HOST,
     port: +(process.env.PG_PORT || 5432),
     username: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
+    password: `${process.env.PG_PASSWORD}`,
     database: process.env.PG_DATABASE,
+    migrations: [__dirname + `/../migrations/**/*.${isProduction ? 'js' : 'ts'}`],
     entities: [__dirname + `/../models/**/*.${isProduction ? 'js' : 'ts'}`],
     synchronize: false,
 })
